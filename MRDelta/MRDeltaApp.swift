d3c72511442a7e1393295a1ff97ae9c9e4e807f7
Jiarui Michelle Xie <michelle.xie111@gmail.com>
2024-07-17 21:24:28 -0500

    Initial Commit

---
+++ MRDelta/MRDeltaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MRDeltaApp.swift
+//  MRDelta
+//
+//  Created by Zhifu Xie on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct MRDeltaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
